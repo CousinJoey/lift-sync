2c7a8e1eb18979990a42c83d242b9e41e9d6694e
CousinJoey <cousinjoeyllc@gmail.com>
2023-09-21 14:55:34 -0400

    foundation homepage

---
+++ lift-sync/src/app/homepage/homepage.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+})
+export class HomepageComponent {
+  constructor(private router: Router) {}
+
+  signOut(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
