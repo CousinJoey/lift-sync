1832881400e0eba11bedae8bb20bea2669eb95ef
CousinJoey <cousinjoeyllc@gmail.com>
2023-09-21 14:59:56 -0400

    routing logic for website

--- lift-sync/src/app/app-routing.module.ts
+++ lift-sync/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomepageComponent, canActivate: [AuthGuard] },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
